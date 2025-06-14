3f2f8204081291c5d21af759d7e3464879a40adc
Nazar <nazargorodyko@gmail.com>
2025-06-14 20:50:33 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,51 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import get_db, MovieModel
-
+from database.models import MovieModel
+from database.session import get_db
+from schemas import MovieDetailResponseSchema
+from schemas.movies import MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movie_list(
+        page: Annotated[int, Query(ge=1)] = 1,
+        per_page: Annotated[int, Query(ge=1, le=20)] = 10,
+        db: AsyncSession = Depends(get_db)
+):
+    quantity = await db.execute(select(func.count(MovieModel.id)))
+    total_items = quantity.scalar()
+
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = (total_items + per_page - 1) // per_page
+    if page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    offset = (page - 1) * per_page
+
+    movie_per_page = await db.execute(select(MovieModel).offset(offset).limit(per_page))
+    movie_list = movie_per_page.scalars().all()
+
+    return MovieListResponseSchema(
+        movies=movie_list,
+        prev_page=f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None,
+        next_page=f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+    return movie
